24c5b8f38edf48e0c01500ed1ef16dfc494bfe6e
MisterKayCodes <kaycris10@gmail.com>
2026-03-26 12:18:22 +0100

    fix: Register TaskIntents in bot/intents/__init__.py

--- bot/intents/__init__.py
+++ bot/intents/__init__.py
@@ -1,5 +1,6 @@
 from bot.intents.activity import ActivityIntents
 from bot.intents.social import SocialIntents
 from bot.intents.system import SystemIntents
+from bot.intents.task_intents import TaskIntents
 
-__all__ = ["ActivityIntents", "SocialIntents", "SystemIntents"]
+__all__ = ["ActivityIntents", "SocialIntents", "SystemIntents", "TaskIntents"]
